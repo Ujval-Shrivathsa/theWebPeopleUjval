828610169b32f8f01ce3bc401c0de6ac0a605cb0
Ujval-Shrivathsa <129066873+Ujval-Shrivathsa@users.noreply.github.com>
2024-03-26 12:30:26 +0530

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,117 @@
+function openModal(){
+    const video = document.querySelector('.iframeVideo');
+    const close = document.querySelector('.thirdContainer');
+    close.style.display = 'none';
+    video.style.display = 'block';
+}
+
+function closeModal(){
+    const video = document.querySelector('.iframeVideo');
+    const close = document.querySelector('.thirdContainer');
+    close.style.display = 'block';
+    video.style.display = 'none';
+}
+
+function showExpand(){
+    const expand = document.querySelector('.expandButton');
+    expand.style.display = 'contents';
+}
+
+function closeExpand(){
+    const expand = document.querySelector('.expandButton');
+    expand.style.display = 'none';
+}
+
+function showExpand2(){
+    const expand = document.querySelector('.expansiveButton');
+    expand.style.display = 'inline';
+}
+
+function closeExpand2(){
+    const expand = document.querySelector('.expansiveButton');
+    expand.style.display = 'none';
+}
+
+function showExpand3(){
+    const expand = document.querySelector('.expansiveButton2');
+    expand.style.display = 'inline';
+}
+
+function closeExpand3(){
+    const expand = document.querySelector('.expansiveButton2');
+    expand.style.display = 'none';
+}
+
+function showExpand4(){
+    const expand = document.querySelector('.expansiveButton3');
+    expand.style.display = 'inline';
+}
+
+function closeExpand4(){
+    const expand = document.querySelector('.expansiveButton3');
+    expand.style.display = 'none';
+}
+
+function showExpand5(){
+    const expand = document.querySelector('.expansiveButton4');
+    expand.style.display = 'inline';
+}
+
+function closeExpand5(){
+    const expand = document.querySelector('.expansiveButton4');
+    expand.style.display = 'none';
+}
+
+function showExpand6(){
+    const expand = document.querySelector('.expansiveButton5');
+    expand.style.display = 'inline';
+}
+
+function closeExpand6(){
+    const expand = document.querySelector('.expansiveButton5');
+    expand.style.display = 'none';
+}
+
+function showExpand7(){
+    const expand = document.querySelector('.expansiveButton6');
+    expand.style.display = 'inline';
+}
+
+function closeExpand7(){
+    const expand = document.querySelector('.expansiveButton6');
+    expand.style.display = 'none';
+}
+
+function showExpand8(){
+    const expand = document.querySelector('.expansiveButton7');
+    expand.style.display = 'inline';
+}
+
+function closeExpand8(){
+    const expand = document.querySelector('.expansiveButton7');
+    expand.style.display = 'none';
+}
+
+function showExpand9(){
+    const expand = document.querySelector('.expansiveButton8');
+    expand.style.display = 'inline';
+}
+
+function closeExpand9(){
+    const expand = document.querySelector('.expansiveButton8');
+    expand.style.display = 'none';
+}
+
+function runfunctio(){
+    const displayy = document.querySelector('.zeropage');
+    const nav = document.querySelector('.navvv');
+    nav.style.display = 'none';
+    displayy.style.display = 'flex';
+}
+
+function runfunc(){
+    const displayy = document.querySelector('.zeropage');
+    const nav = document.querySelector('.navvv');
+    nav.style.display = 'flex';
+    displayy.style.display = 'none';
+}
